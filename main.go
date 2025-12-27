4088f19cc611e7650bf114a6e8203dcddc6f5fca
srdarknighter <anirudhiyer05@gmail.com>
2025-12-27 15:53:51 +0530

    feat: Basic Container Runtime

---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strconv"
+	"syscall"
+)
+
+func main() {
+	switch os.Args[1] {
+	case "run":
+		run()
+	case "child":
+		child()
+	default:
+		panic("bad command")
+	}
+}
+
+func run() {
+	fmt.Printf("Running %v\n", os.Args[2:])
+
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags:   syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+		Unshareflags: syscall.CLONE_NEWNS,
+	}
+
+	if err := cmd.Run(); err != nil {
+		panic(err)
+	}
+}
+
+func child() {
+	fmt.Printf("Running %v\n", os.Args[2:])
+
+	if err := syscall.Sethostname([]byte("container")); err != nil {
+		panic(err)
+	}
+
+	syscall.Chroot("/home/{hostname}/ubuntu-fs")
+	syscall.Chdir("/")
+	syscall.Mount("proc", "proc", "proc", 0, "")
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	cgv1()
+
+	if err := cmd.Run(); err != nil {
+		panic(err)
+	}
+
+	syscall.Unmount("/proc", 0)
+}
+
+func cgv1() {
+	cgroups := "/sys/fs/cgroup/"
+	pids := filepath.Join(cgroups, "pids")
+	os.MkdirAll(filepath.Join(pids, "{hostname}"), 0755)
+	must(os.WriteFile(filepath.Join(pids, "{hostname}/pids.max"), []byte("20"), 0700))
+	must(os.WriteFile(filepath.Join(pids, "{hostname}/notify_on_release"), []byte("1"), 0700))
+	must(os.WriteFile(filepath.Join(pids, "{hostname}/cgroup.procs"), []byte(strconv.Itoa(os.Getpid())), 0700))
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
